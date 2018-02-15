27e0136508324c459cf6e6a707bc7bfa5422beb3
Maxime Rainville <maxime@webtorque.co.nz>
2018-02-15 17:37:17 +1300

    Get rid of all the debug code switch to onAfterVersionedPublish.

--- src/Blocks/AutoPublishElementalExtension.php
+++ src/Blocks/AutoPublishElementalExtension.php
@@ -6,13 +6,12 @@ use SilverStripe\ORM\DataExtension;
 use SilverStripe\Versioned\Versioned;
 use DNADesign\Elemental\Models\ElementalArea;
 use WebTorque\SilverstripeHelpers\Tests\Mocks\DummyPage;
+use WebTorque\SilverstripeHelpers\Tests\Mocks\DummyElement;
 
 /**
  * Extends a SiteTree to auto publish all its elemental blocks when the it's published.
  *
- * You can define the following flags on your class to control this behavior:
- * * `auto_publish_elemental_fields`: array of string reprensenting the expected names of the Elemental field. Defaults
- * to `ElementalArea` if not set.
+ * You can define the following flag on your class to control this behavior:
  * * `auto_publish_elemental_disable`: Allows you to disable the auto publishing feature in child classes. Default to
  * `false`.
  *
@@ -23,15 +22,13 @@ class AutoPublishElementalExtension extends DataExtension
     /**
      * [onAfterWrite description]
      */
-    public function onAfterWrite(): void
+    public function onAfterVersionedPublish($fromStage, $toStage, $createNewVersion): void
     {
-        // If current page is a draft or if auto-publishing is disable, quit right away.
-        $liveID = Versioned::get_versionnumber_by_stage($this->getOwner(), Versioned::LIVE, $this->getOwner()->ID);
-        if ($this->getOwner()->Version != $liveID  || $this->getAutoPublishElementalDisable()) {
+        if ($toStage != Versioned::LIVE  || $this->getAutoPublishElementalDisable()) {
             return;
         }
 
-        $fields = $this->getAutoPublishElementalFields();
+        $fields = $this->getOwner()->getElementalRelations();
         foreach ($fields as $fieldName) {
             $this->autopublish($fieldName);
         }
@@ -49,39 +46,12 @@ class AutoPublishElementalExtension extends DataExtension
         }
 
         $elementArea = $this->getOwner()->{$fieldName}();
-        $elementArea = $this->getOwner()->ElementalArea();
-
-        var_dump(ElementalArea::singleton()->baseTable());
-        die();
-        foreach (ElementalArea::get() as $area) {
-            var_dump($area->ID);
-        }
-
 
         if ($elementArea) {
             $elements = $elementArea->Elements();
             foreach ($elements as $element) {
-                echo "publishing element";
                 $element->copyVersionToStage(Versioned::DRAFT, Versioned::LIVE);
             }
-        }
-    }
-
-    /**
-     * Get the list of Elemental fields that should be auto published. This can be defined on the parent class with the
-     * `auto_publish_elemental_fields` config flag as a string or array of string. If left unset, the value defaults to
-     * `ElementalArea`.
-     * @return string[]
-     */
-    public function getAutoPublishElementalFields(): array
-    {
-        $fields = $this->getOwner()->config()->get('auto_publish_elemental_fields');
-        if (!$fields) {
-            return ['ElementalArea'];
-        } elseif (is_array($fields)) {
-            return $fields;
-        } else {
-            return [$fields];
         }
     }
 
@@ -95,5 +65,10 @@ class AutoPublishElementalExtension extends DataExtension
         return
             $this->getOwner()->config()->get('auto_publish_elemental_disable') ?
             true : false;
+    }
+
+    public function countElement()
+    {
+        return ElementalArea::get()->count();
     }
 }
