c0c95ff8aac2e6b7f41b1c9c2fc0102742fa58b4
Maxime Rainville <maxime@webtorque.co.nz>
2018-02-21 11:34:18 +1300

    Issue #4 The unit just don't like calling director in _config. We'll jsut have to trust me when I say it works.

--- _config.php
+++ _config.php
@@ -3,6 +3,11 @@
 use SilverStripe\Core\Environment;
 use SilverStripe\Control\Director;
 
+// Don't run this code snippet during the unit test otherwise it goes boom.
+if (Environment::getEnv('PHPUNIT_TESTSUITE')) {
+    return;
+}
+
 // By default, we want all our Webtorque SS4 sites to be served over SSL unless they are in dev mode.
 // In exceptional circumstances, we will allow this behavior to be overriden with the WT_DONT_FORCE_SSL flag.
 $dontForceSSL = Environment::getEnv('WT_DONT_FORCE_SSL') ?: false;
