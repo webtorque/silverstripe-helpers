e5fa3b523ac779980dc718c30933c8e235b2e88d
Maxime Rainville <maxime@webtorque.co.nz>
2018-02-21 12:33:35 +1300

    Issue #4 Move the force SSL logic to a class outside of _config and restore unit test.

--- _config.php
+++ _config.php
@@ -1,17 +1,11 @@
 <?php
 
-use SilverStripe\Core\Environment;
-use SilverStripe\Control\Director;
+use SilverStripe\Core\Environment as SSEnvironment;
+use WebTorque\SilverstripeHelpers\Environment;
 
 // Don't run this code snippet during the unit test otherwise it goes boom.
-if (Environment::getEnv('PHPUNIT_TESTSUITE')) {
+if (SSEnvironment::getEnv('PHPUNIT_TESTSUITE')) {
     return;
 }
 
-// By default, we want all our Webtorque SS4 sites to be served over SSL unless they are in dev mode.
-// In exceptional circumstances, we will allow this behavior to be overriden with the WT_DONT_FORCE_SSL flag.
-$dontForceSSL = Environment::getEnv('WT_DONT_FORCE_SSL') ?: false;
-
-if (!$dontForceSSL && !Director::isDev()) {
-    Director::forceSSL();
-}
+Environment::ForceSSL();
