8413d1967e6b63978539f66147df817ba4ee7f23
Maxime Rainville <maxime@webtorque.co.nz>
2018-02-21 10:02:30 +1300

    Issue #4 - Add some rules to force SSL in UAT and Live.

--- _config.php
+++ _config.php
@@ -1,1 +1,12 @@
 <?php
+
+use SilverStripe\Core\Environment;
+use SilverStripe\Control\Director;
+
+// By default, we want all our Webtorque SS4 sites to be served over SSL unless they are in dev mode.
+// In exceptional circumstances, we will allow this behavior to be overriden with the WT_DONT_FORCE_SSL flag.
+$dontForceSSL = Environment::getEnv('WT_DONT_FORCE_SSL') ?: false;
+
+if (!$dontForceSSL && !Director::isDev()) {
+    Director::forceSSL();
+}
